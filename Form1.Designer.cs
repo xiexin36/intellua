7512f4f187cf12c1749460b2362ba404871028b5
AzureBlaze <AzureBlaze@localhost>
2013-01-21 20:51:44 +0800

    start

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace LuaEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scintilla1 = new ScintillaNET.Scintilla();
+            ((System.ComponentModel.ISupportInitialize)(this.scintilla1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scintilla1
+            // 
+            this.scintilla1.Location = new System.Drawing.Point(54, 12);
+            this.scintilla1.Name = "scintilla1";
+            this.scintilla1.Size = new System.Drawing.Size(587, 375);
+            this.scintilla1.Styles.BraceBad.Size = 9F;
+            this.scintilla1.Styles.BraceLight.Size = 9F;
+            this.scintilla1.Styles.ControlChar.Size = 9F;
+            this.scintilla1.Styles.Default.BackColor = System.Drawing.SystemColors.Window;
+            this.scintilla1.Styles.Default.Size = 9F;
+            this.scintilla1.Styles.IndentGuide.Size = 9F;
+            this.scintilla1.Styles.LastPredefined.Size = 9F;
+            this.scintilla1.Styles.LineNumber.Size = 9F;
+            this.scintilla1.Styles.Max.Size = 9F;
+            this.scintilla1.TabIndex = 0;
+            this.scintilla1.CharAdded += new System.EventHandler<ScintillaNET.CharAddedEventArgs>(this.scintilla1_CharAdded);
+            this.scintilla1.TextDeleted += new System.EventHandler<ScintillaNET.TextModifiedEventArgs>(this.scintilla1_TextDeleted);
+            this.scintilla1.TextInserted += new System.EventHandler<ScintillaNET.TextModifiedEventArgs>(this.scintilla1_TextInserted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 432);
+            this.Controls.Add(this.scintilla1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.scintilla1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ScintillaNET.Scintilla scintilla1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace LuaEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            scintilla1.ConfigurationManager.CustomLocation = "ScintillaNET.xml";
+            scintilla1.ConfigurationManager.Language = "lua";
+            scintilla1.ConfigurationManager.Configure();
+            scintilla1.Folding.IsEnabled = true;
+            scintilla1.Margins[0].Width = 20;
+            scintilla1.Margins[1].Width = 20;
+            scintilla1.Margins[2].Width = 20;
+        }
+
+        private void scintilla1_CharAdded(object sender, ScintillaNET.CharAddedEventArgs e)
+        {
+            if (e.Ch == '.')
+            {
+                int start = scintilla1.Lines.Current.StartPosition;
+                int end = scintilla1.CurrentPos;
+                string str = scintilla1.Text.Substring(start, end - start - 1);
+                Regex rex = new Regex(@"(?:.*?)(?<word>[\w.:\(\)]+$)");
+                Match m = rex.Match(str);
+                if (m.Success)
+                {
+                    string lw = m.Groups["word"].Value;
+                    scintilla1.CallTip.Show(lw);
+                }
+            }
+        }
+
+        private void parseLine(string line) {
+            /*const string expr = @"(?<var>\w+)\s*=\s*(?<exp>.+)((?=\W$)|\z)";
+            Regex rex = new Regex(expr);
+            Match m = rex.Match(line);
+            if(m.Success){
+                string var = m.Groups["var"].Value;
+                string exp = m.Groups["exp"].Value;
+                 
+                scintilla1.CallTip.Show(var + " = " + exp);
+                System.Diagnostics.Debug.Print(var + " = " + exp);
+            }*/
+        }
+
+        private void scintilla1_TextDeleted(object sender, ScintillaNET.TextModifiedEventArgs e)
+        {
+            parseLine(scintilla1.Lines.Current.Text);
+        }
+
+        private void scintilla1_TextInserted(object sender, ScintillaNET.TextModifiedEventArgs e)
+        {
+            parseLine(scintilla1.Lines.Current.Text);
+        }
+    }
+}
